4f07c1563ca5b5f44f0ae3f601f238ebf30f95a1
tc2602 <96946644+tc2602@users.noreply.github.com>
2022-03-13 14:33:57 +0700

    UpdateServlet

--- BookingHotelsAssignment/src/java/controller/UpdateServlet.java
+++ BookingHotelsAssignment/src/java/controller/UpdateServlet.java
@@ -5,12 +5,16 @@
  */
 package controller;
 
+import dal.DAO;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.Homestay;
+import model.HomestayType;
 
 /**
  *
@@ -70,7 +74,16 @@ public class UpdateServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        int id = Integer.parseInt(request.getParameter("id"));
+        
+        DAO db = new DAO();
+        Homestay h = db.getHomestayById(id);
+        request.setAttribute("homestay", h);
+        
+        List<HomestayType> listType = db.getAllType();
+        request.setAttribute("listType", listType);
+        
+        request.getRequestDispatcher("homestayupdate.jsp").forward(request, response);
     }
 
     /**
